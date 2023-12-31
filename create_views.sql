88b9159c9350bd09dcabfe6e2f6e0fd150da9211
StefanFotin23 <andreifotin@gmail.com>
2023-12-31 17:29:12 +0200

    Reports have right complexity

--- create_views.sql
+++ create_views.sql
@@ -1,58 +1,55 @@
 -- Rapoarte
--- Complexitate 5
--- Cantitatea totala vanduta pentru fiecare categorie de produse
-CREATE VIEW VANZARI_TOTALE_CATEGORIE AS
-SELECT
-    cp.nume AS CATEGORIE,
-    SUM(vp.cantitate) AS "CANTITATE TOTALA",
-    SUM(vp.cantitate * sp.pret) AS "VALOARE TOTALA (lei)",
-    CASE
-        WHEN SUM(vp.cantitate) > 0
-            THEN ROUND(SUM(vp.cantitate * sp.pret) / SUM(vp.cantitate))
-        ELSE 0
-    END AS "PRET MEDIU PE UNITATE (lei)"
-FROM
-    categorii_produse cp
-JOIN stoc_produse sp ON cp.id = sp.id_categorie
-JOIN vanzari_produse vp ON sp.id = vp.id_produs
-GROUP BY cp.nume;
 
--- Complexitate 7
--- Vanzarile lunare, inclusiv comisioanele si discounturile aplicate
-CREATE VIEW VANZARI_LUNARE_CU_COMISION AS
-SELECT TO_CHAR(vp.data_vanzare, 'YYYY-MM') AS LUNA,
-       a.nume AS "NUME ANGAJAT",
-       cp.nume AS "CATEGORIE",
-       vp.cantitate,
-       ROUND (vp.cantitate * sp.pret * (1 - dcc.valoare_discount) * (1 + dcc.valoare_comision)) AS "venit (lei)"
-FROM vanzari_produse vp
+-- Complexitate 4
+-- Produsele vandute de fiecare angajat, inclusiv categoria produsului
+CREATE VIEW VANZARI_ANGAJAT_CATEGORIE AS
+SELECT
+    a.nume AS "NUME ANGAJAT",
+    cp.nume AS "CATEGORIE PRODUS",
+    COUNT(vp.id_produs) AS "NUMAR PRODUSE VANDUTE"
+FROM
+    vanzari_produse vp
 JOIN stoc_produse sp ON vp.id_produs = sp.id
 JOIN angajati a ON vp.id_angajat = a.id
 JOIN categorii_produse cp ON sp.id_categorie = cp.id
-LEFT JOIN discount_si_comision_categorie dcc ON cp.id = dcc.id_categorie
-ORDER BY luna;
+GROUP BY a.nume, cp.nume;
 
--- Complexitate 8
--- Produsele disponibile in stoc, inclusiv discounturile aplicate
--- Complexitate 8
--- Produsele disponibile în stoc, inclusiv discounturile aplicate
-CREATE VIEW PRODUSE_CU_DISCOUNT AS
-SELECT cp.nume AS CATEGORIE,
-       sp.putere_termica_kWh,
-       sp.randament_energetic,
-       sp.cantitate,
-       sp.pret,
-       CASE
-           WHEN dcc.valoare_discount IS NOT NULL AND dcc.valoare_discount > 0
-               THEN sp.pret * dcc.valoare_discount
-           ELSE 0
-       END AS "DISCOUNT APLICAT (lei)",
-       sp.pret - (CASE
-                     WHEN dcc.valoare_discount IS NOT NULL AND dcc.valoare_discount > 0
-                         THEN sp.pret * dcc.valoare_discount
-                     ELSE 0
-                 END) AS "PRET FINAL (lei)"
-FROM stoc_produse sp
+-- Complexitate 6
+-- Statistica vanzari pe fiecare categorie
+CREATE VIEW INFORMATII_VANZARI_CATEGORII AS
+SELECT
+    cp.nume AS "CATEGORIE",
+    COUNT(DISTINCT vp.id) AS "NUMAR VANZARI",
+    ROUND(AVG(sp.pret)) AS "PRET MEDIU",
+    SUM(vp.cantitate) AS "CANTITATE TOTALA VANDUTA",
+    SUM(vp.cantitate * sp.pret) AS "SUMA TOTALA VANDUTA"
+FROM
+    vanzari_produse vp
+JOIN stoc_produse sp ON vp.id_produs = sp.id
 JOIN categorii_produse cp ON sp.id_categorie = cp.id
-LEFT JOIN discount_si_comision_categorie dcc ON cp.id = dcc.id_categorie;
-COMMIT;
+LEFT JOIN discount_si_comision_categorie dcc ON cp.id = dcc.id_categorie
+WHERE vp.data_vanzare BETWEEN TO_DATE('2023-01-01', 'YYYY-MM-DD') AND TO_DATE('2023-12-31', 'YYYY-MM-DD')
+GROUP BY cp.nume
+HAVING SUM(vp.cantitate) > 10;
+
+-- Complexitate 7
+-- Statistica despre performanta angajatilor in vanzari
+CREATE VIEW PERFORMANTA_ANGAJATI_VANZARI AS
+SELECT
+    a.nume AS "NUME ANGAJAT",
+    a.prenume AS "PRENUME ANGAJAT",
+    COUNT(vp.id) AS "NUMAR VANZARI",
+    SUM(vp.cantitate) AS "CANTITATE TOTALA VANDUTA",
+    ROUND(SUM(sp.pret * (1 - dcc.valoare_discount) * (1 + dcc.valoare_comision)), 2) AS "VENIT TOTAL (lei)",
+    AVG(dcc.valoare_discount) AS "DISCOUNT MEDIU",
+    AVG(dcc.valoare_comision) AS "COMISION_MEDIU"
+FROM
+    angajati a
+JOIN vanzari_produse vp ON a.id = vp.id_angajat
+JOIN stoc_produse sp ON vp.id_produs = sp.id
+LEFT JOIN discount_si_comision_categorie dcc ON sp.id_categorie = dcc.id_categorie
+JOIN categorii_produse cp ON sp.id_categorie = cp.id
+WHERE a.data_angajare <= TO_DATE('2023-12-31', 'YYYY-MM-DD')
+GROUP BY a.nume, a.prenume, (dcc.valoare_discount + dcc.valoare_comision) / 2
+HAVING AVG(dcc.valoare_discount) <= 0.1 AND AVG(dcc.valoare_comision) <= 0.1
+ORDER BY (dcc.valoare_discount + dcc.valoare_comision) / 2 ASC;
